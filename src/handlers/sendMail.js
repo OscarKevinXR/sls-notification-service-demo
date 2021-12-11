b8d0685b4fba84755a045e7d3baaaf46df6f7db2
Ousikai <mtfallsVR@protonmail.com>
2021-12-11 16:40:19 -0500

    Provising AWS SQS Message Queue

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-1' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'ohaiyo@atemosta.com',
     Destination: {
-      ToAddresses: ['ohaiyo@atemosta.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from Atemosta!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
